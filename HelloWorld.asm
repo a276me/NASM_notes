353af8e1effbbf7c516a6fa7c0b57008f084c12c
a276me <a276me@users.noreply.github.com>
2021-05-20 19:22:43 +0800

    Helloworld

---
+++ HelloWorld.asm
@@ -0,0 +1,20 @@
+
+global _start
+
+section .data
+	msg db "Hello World"
+	len equ $-msg
+
+section .text
+	_start:
+
+		MOV EAX, 4
+		MOV EBX, 1
+		MOV ECX, msg
+		MOV EDX, len
+		INT 0x80
+
+		MOV EAX, 1 
+		MOV EBX, 0
+		INT 0x80
+
